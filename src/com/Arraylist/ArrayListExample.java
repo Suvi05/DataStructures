458f846e3e31340df271900d97c06b6799300ee5
suvi05 <suv143ak@gmail.com>
2020-05-12 00:37:18 +0530

    ArrayList's Examples added

---
+++ src/com/Arraylist/ArrayListExample.java
@@ -0,0 +1,23 @@
+package com.Arraylist;
+
+import java.util.ArrayList;
+
+public class ArrayListExample {
+    public static void main(String[] args) {
+
+        int[] intArray = {20, 10, 5, 4, 7};
+
+        System.out.println("Elements in array are :");
+        for (int i = 0; i < intArray.length; i++) {
+            System.out.println(intArray[i]);
+        }
+
+        ArrayList<String> list= new ArrayList<>();
+        list.add("Sumit");
+        list.add("Verma");
+        list.remove(1);
+        list.add("Krati");
+
+        System.out.println(list);
+    }
+}
---
+++ src/com/Arraylist/main.java
@@ -0,0 +1,32 @@
+package com.Arraylist;
+
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class main {
+    public static void main(String[] args) {
+
+        List<Employee> employeeList =new ArrayList<>();
+        //List is the parent class of ArrayList
+        //Default size of Arraylist is 10
+
+        employeeList.add(new Employee("Sumit", "Verma", 101));
+        employeeList.add(new Employee("Krati", "Taneja", 102));
+        employeeList.add(new Employee("Akanksha", "Ranawat", 103));
+        employeeList.add(new Employee("Tanuja", "Sharma", 104));
+
+        employeeList.forEach(employee -> System.out.println(employee));
+        // -> lambda... and that is a part of java8
+
+        System.out.println(employeeList.get(1));
+
+        //employeeList.clear();
+
+        System.out.println(employeeList.isEmpty());
+        employeeList.set(2,new Employee("Ananta", "Sharma",103));
+        System.out.println(employeeList);
+        System.out.println(employeeList.size());//capacity
+    }
+}
